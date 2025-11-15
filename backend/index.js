1b918e835c8ab7342d3310448a4a2ec802fe2de5
bidisha1005 <153932857+bidisha1005@users.noreply.github.com>
2025-11-16 00:03:00 +0530

    remove password

--- backend/index.js
+++ backend/index.js
@@ -10,7 +10,7 @@ app.use(cors());
 const db = mysql.createConnection({
     host: "localhost",
     user: "root",
-    password: "tw1ns@10",
+    password: " ",
     database: "rental_management"
 });
 
