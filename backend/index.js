a4ca3732947ccff27e3e65cc8762e5bd94531eb6
Anushka-Mandal <165414511+Anushka-Mandal@users.noreply.github.com>
2026-01-29 22:27:26 +0530

    Change default database name to 'railway'

--- backend/index.js
+++ backend/index.js
@@ -11,7 +11,7 @@ const pool = mysql.createPool({
     host: process.env.MYSQL_HOST || "localhost",
     user: process.env.MYSQL_USER || "root",
     password: process.env.MYSQL_PASSWORD || " ",
-    database: process.env.MYSQL_DATABASE || "rental_management",
+    database: process.env.MYSQL_DATABASE || "railway",
     port: process.env.MYSQL_PORT || 3306,
     waitForConnections: true,
     connectionLimit: 10,
